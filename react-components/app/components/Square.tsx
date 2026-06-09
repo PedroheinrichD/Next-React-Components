201320c6a9bbf5dfb53f466f94cad505e5b25073
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-06-09 15:48:28 -0300

    feat: exporta e importa componentes

---
+++ react-components/app/components/Square.tsx
@@ -0,0 +1,13 @@
+export const Square = () =>{
+    return <>
+        <section className="w-52 h-52 bg-amber-700 text-white">
+            <h2>Quadrado insano sendo importado</h2>
+        </section>
+    </>
+}
+
+export const Circle = () => {
+    return <>
+        <div className="w-52 h-52 bg-amber-200 rounded-full"></div>    
+    </>
+}
--- react-components/app/page.tsx
+++ react-components/app/page.tsx
@@ -1,7 +1,11 @@
+import { Square, Circle } from "./components/Square";
+
 const Page = () =>{
   return <>
       <section className="bg-sky-200 text-white">
-        <h1>Olá mundo</h1>
+        <h1>exportanto e importando components</h1>
+        <Square />
+        <Circle />
       </section>
   </>
 }
