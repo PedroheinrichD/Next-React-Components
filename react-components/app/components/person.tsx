81513e75be370068e885bcdd84abcde553952f23
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-06-13 08:39:57 -0300

    feat: Implementa valor padrão em props opcionais

--- react-components/app/components/person.tsx
+++ react-components/app/components/person.tsx
@@ -1,16 +1,22 @@
-
-
 type Props = {
   name: string;
-  address: string
+  address: string;
+  avatar?: string;
 };
 
-export const Person = ({name, address}: Props) => {
-
+export const Person = ({
+  name,
+  address,
+  avatar = "https://png.pngtree.com/element_our/20200610/ourlarge/pngtree-black-default-avatar-image_2237212.jpg",
+}: Props) => {
   return (
     <>
-      <h1>Seja bem vindo {name} que mora na rua {address}</h1>
-    
+      <section>
+        <h1>
+          Seja bem vindo {name} que mora na rua {address}
+        </h1>
+        <img className="h-35" src={avatar} alt="" />
+      </section>
     </>
   );
 };
--- react-components/app/page.tsx
+++ react-components/app/page.tsx
@@ -7,6 +7,7 @@ const Page = () => {
         <Person 
           name="Keyson"
           address="brasil"
+          avatar="https://img.magnific.com/fotos-gratis/jovem-homem-posando-isolado-contra-a-parede-em-branco-do-estudio_273609-12356.jpg?semt=ais_hybrid&w=740&q=80"
         />
 
         <Person
