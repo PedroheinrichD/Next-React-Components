b493d38305496e323b6ebfe37c7ec3b3dceb6bb8
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-06-17 07:24:48 -0300

    feat: Adiciona filtro de busca para tais profissões

--- react-components/app/page.tsx
+++ react-components/app/page.tsx
@@ -2,12 +2,14 @@ import { PeopleList } from "../data/PeopleList";
 
 const Page = () => {
 
+  const busca = PeopleList.filter(item => item.profession === 'DEV');
   return (
     <>
       <section className="flex flex-col gap-10">
       <ul>
-       {PeopleList.map(
-        person => <li key={person.id}> {person.name} / {person.profession}</li>
+        <h3>Lista de DEVS</h3>
+        {busca.map(item  =>
+          <li key={item.id}>{item.name}</li>
         )}
       </ul>
       </section>
