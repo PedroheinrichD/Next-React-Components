a743310e1ea62d15ecd8a877a24b37c497e74632
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-06-22 11:35:54 -0300

    feat: filtra produtos com preço maiores que 100

--- react-components/app/page.tsx
+++ react-components/app/page.tsx
@@ -1,16 +1,20 @@
-import { PeopleList } from "../data/PeopleList";
-
 const Page = () => {
-
-  const busca = PeopleList.filter(item => item.profession === 'DEV');
+  const produtos = [
+    {id: 1, name: 'Mouse', preço:50},
+    {id: 2, name: 'Teclado', preço:100},
+    {id: 3, name: 'Monitor', preço:800},
+    {id: 4, name: 'Webcam', preço:150},
+  ]
+  // mostrar produtos e seus preços
   return (
     <>
       <section className="flex flex-col gap-10">
       <ul>
-        <h3>Lista de DEVS</h3>
-        {busca.map(item  =>
-          <li key={item.id}>{item.name}</li>
-        )}
+        <h3>Lista de produtos</h3>
+        {produtos.filter(item => item.preço > 100)
+        .map((item)=>(
+          <li key={item.id}> {item.name} - R${item.preço}</li>
+        ))}
       </ul>
       </section>
     </>
