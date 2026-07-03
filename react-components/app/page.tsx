7808f04a4c2a527a6ccf0f6469dca41f3cb41a64
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-07-03 06:31:14 -0300

    feat: Atualiza objeto em state

--- react-components/app/page.tsx
+++ react-components/app/page.tsx
@@ -1,24 +1,33 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
 const Page = () => {
-
-  const [count, setCount] = useState(0)
-
-  function handleButton(){
-    setCount(count + 2)
-  }
+  const [fullName, setFullName] = useState({ name: "", lastName: "" });
 
   return (
-    <section className="p-6 mx-auto text-center">
-      <p className="text-3xl">{count}</p>
-      <button
-        className="py-2 px-4 rounded bg-sky-500 text-white cursor-pointer"
-        onClick={handleButton}
-      >
-        +2
-      </button>
+    <section className="p-6 mx-auto text-center space-y-4">
+      <div>
+        <input
+          placeholder="digite seu nome"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
+          value={fullName.name}
+          onChange={e =>setFullName({...fullName, name: e.target.value})}
+        />
+        
+      </div>
+      <div>
+        <input
+          placeholder="digite seu sobrenome"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
+          value={fullName.lastName}
+           onChange={e =>setFullName({...fullName, lastName: e.target.value})}
+        />
+        
+      </div>
+
+      <p>Seu nome completo é:</p>
+      <p>{fullName.name} {fullName.lastName}</p>
     </section>
   );
 };
