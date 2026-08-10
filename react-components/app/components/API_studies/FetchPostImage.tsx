41213cc1f9716e010634df467adf59b6482e7416
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-08-10 05:58:21 -0300

    feat: cria novo arquivo para envio imagens

---
+++ react-components/app/components/API_studies/FetchPostImage.tsx
@@ -0,0 +1,7 @@
+export function FetchPostImage() {
+    return(
+        <section>
+            
+        </section>
+    )
+}
--- react-components/app/page.tsx
+++ react-components/app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { FetchAPI } from "./components/API_studies/FetchAPI";
-import { FetchPOST } from "./components/API_studies/FetchPOST";
-
 const Page = () => {
   return (
     <section className="container mx-auto">
-      <FetchAPI/>
-      <FetchPOST/>
     </section>
   );
 };
