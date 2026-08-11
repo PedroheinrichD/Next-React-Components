622f110e7fc92878a2c2fb1e5da7d3618b913289
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-08-11 18:59:37 -0300

    feat: Adiciona abortController para cancelar a requisição

--- react-components/app/components/AXIOS/AxiosGET.tsx
+++ react-components/app/components/AXIOS/AxiosGET.tsx
@@ -1,30 +1,53 @@
-import { api } from "@/utils/api"
+import { api } from "@/utils/api";
+import { useRef, useState } from "react";
 
 export function AxiosGET() {
-    // exemplo de uso do axios para fazer uma requisição GET
-    async function handleGetRequest() {
-        const res = await api.get('/posts', {
-            params: {
-                userId: 1
-            }
-        })
-        console.log(res.data) // em data fica a resposta da requisição, no caso, um array de posts do usuário com id 1
+  const [error, setError] = useState("")
+  const controllerRef = useRef<AbortController | null>(null) // usando como ref para guardar o valor quando re-renderizar 
+  
+  // exemplo de uso do axios para fazer uma requisição GET
+  async function handleGetRequest() {
+    if(controllerRef.current){ // Se existe uma requisição anterior rodando, cancela ela
+      controllerRef.current.abort();
     }
-    // se não quiser usar params, pode fazer a requisição com a url normal, parametros nao sao obrigatorios, mas podem ser uteis dependendo do caso
 
-    return (
-        <div className="flex flex-col items-center justify-center gap-4">
-            <h1>Exemplo de uso do Axios</h1>
-            
-             {/*GET*/}
-            <button
-                onClick={handleGetRequest}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-                Fazer requisição GET
-            </button>
-        </div>
-    )
+    controllerRef.current = new AbortController();  // é um recurso do proprio fetch API para cancelar requisições
+    try { 
+      const res = await api.get("/posts", {
+        params: {
+          userId: 1,
+        },
+        signal: controllerRef.current?.signal, // associando o meu controlador ao sinal da requisição
+      });
+      console.log(res.data); // em data fica a resposta da requisição, no caso, um array de posts do usuário com id 1
+    } catch {
+      setError('requisição falhou')
+    }
+  }
+  // se não quiser usar params, pode fazer a requisição com a url normal, parametros nao sao obrigatorios, mas podem ser uteis dependendo do caso
+
+  function handleCancelRequest() {
+    controllerRef.current?.abort();
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <h1>Exemplo de uso do Axios</h1>
+
+      {/*GET*/}
+      <button
+        onClick={handleGetRequest}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Fazer requisição GET
+      </button>
+      <p className="text-red-500">{error}</p>
+
+      <button className="border p-2" onClick={handleCancelRequest}>
+        Cancelar requisição GET
+      </button>
+    </div>
+  );
 }
 
 /*
